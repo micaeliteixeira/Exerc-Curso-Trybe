23048cd831b0d1cba9613fc3eef5740613058872
Micaeli Teixeira <luime6@gmail.com>
2020-07-13 19:41:39 -0300

    Adicionando os exercícios 04,05 e 06

---
+++ exercises/4_1/exer05.js
@@ -0,0 +1,13 @@
+/*Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário.*/
+
+let angleA = 15;
+let angleB = 45;
+let angleC = 120;
+
+let sumAgle = angleA+angleB+angleC;
+
+if (sumAgle!==180) {
+ console.log("Soma dos três ângulos não representa um triangulo");
+}else{
+ console.log("Soma dos três ângulos representa um triangulo");
+};
