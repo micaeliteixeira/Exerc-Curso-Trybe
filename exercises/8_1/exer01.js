e088e76ece9570cfd1e06e7937064bb4848defb5
Micaeli Teixeira <luime6@gmail.com>
2020-08-19 14:16:17 -0300

    exercicio 4

---
+++ exercises/8_1/exer01.js
@@ -0,0 +1,75 @@
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+//Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+function authorBornIn1947() {
+  // essa parte faz a função de validação para o if que vai dentro do find
+  // nameFind com o find procura 1º elemento que é verdade para o if lá dentro e author.name seleciona que quero imprima apenas essa parte
+  const nameBorn = authorName => authorName.author.birthYear == 1947;
+  const nameFind = books.find(nameBorn).author.name;
+  return nameFind;
+}
+console.log (authorBornIn1947(books));
+assert.equal(authorBornIn1947(), 'Stephen King');
